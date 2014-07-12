7f9bf8946d45e151f95cdd96ea19ad1727034a61
Benco <bcos34@gmail.com>
2014-07-12 09:53:09 -0400

    Minor corrections to layout and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,9 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
-    before { visit root_path }
+    before { visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should have_title(full_title('')) }
+    it { should have_title(full_title('Sign up')) }
   end
 end
