ac35337a27fa936c28923b883c88ce4fbf99ed28
Benco <bcos34@gmail.com>
2014-07-19 13:37:02 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+
+
+  def show # GET      /users/1     user_path(user)      page to show user
+  	@user = User.find(params[:id])
+  end
+
+  def new # GET       /users/new   new_user_path        make new user page (signup)      
+  	@user = User.new
+  end
+
+  def create # POST   /users       users_path           create a new user
+  	@user = User.new(user_params) # Not the final implementation
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email,
+  		:password, :password_confirmation)
   end
 end
